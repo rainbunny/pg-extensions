940d20024db76000dbdc9ff500c9d0ea611959e9
Thinh Tran <quangthinhtran3588@gmail.com>
2021-04-16 18:40:35 +0700

    add Database interface to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export type {DbQuery, ExtendedPoolConfig, Logger} from './interfaces';
+export type {DbQuery, ExtendedPoolConfig, Logger, Database} from './interfaces';
 
 export {buildQuery} from './build-query';
 export {executeQuery} from './execute-query';
